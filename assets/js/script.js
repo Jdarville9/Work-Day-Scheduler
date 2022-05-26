3fb861fa85c8aa3ef4ef71b98fd4eee1d9971f13
Joseph Darville <joseph.darville9@gmail.com>
2022-05-26 17:59:19 -0400

    final touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,38 @@
 var todaysDate = moment().format("MMM, DD, YYYY");
 var startTime = moment().set("hour", 6).format("h:mm a")
-var timeBlock = $("#container")
-var saveBtn =
+var textArray = []
 
 setDate = function() {
     $("#currentDay").text(todaysDate);
-}
-
-setDate();
+};
 
 
-// Get Hours For Time Slots
+// compare current time to the numbers
 pasCurFut = function() {
     var currentTime = moment().hour()
    for (var i = 0; i < 12; i++) {
-    var setTime
+    var setTime = i + 6;
     if (currentTime > setTime) {
-
-        console.log("past")
+        $("#textarea" + i).addClass("past")
     } else if (currentTime === setTime) {
-        console.log("present")
+        $("#textarea" + i).addClass("present")
     } else {
-        console.log("poop")
+        $("#textarea" + i).addClass("future")
     }
    } 
 };
 
-var createRows = function() {
+var addStuff = function(){
+    $(".jumbotron").addClass("past")
+};
 
+var createRows = function() {
     for(var i = 0; i < 12; i++) {
         var timeSet = moment(startTime, "h a").add(i, "hour").format("h a");
-        var row = $("<div>").addClass("timeblock row")
+        var row = $("<div>").addClass("timeblock row").attr("id", "timeblock")
         var rowTime = $("<div>").addClass("hour col-1").text(timeSet)
-        var textArea = $("<textarea>").addClass("textarea col-10 past").attr("id", "textarea")
-        var saveButton = $("<button>").addClass("saveBtn col-1").attr("id", "saveBtn").html("<i class='fas fa-save'></i>")
+        var textArea = $("<textarea>").addClass("textarea col-10").attr("id", "textarea" +i).text(textArray[i])
+        var saveButton = $("<button>").addClass("saveBtn col-1").attr("id", "saveBtn" +i).html("<i class='fas fa-save'></i>")
         var icon = $("<i>").addClass("bi bi-save-fill")
 
         row.append(rowTime, textArea, saveButton);
@@ -41,13 +40,39 @@ var createRows = function() {
         saveButton.append(icon);
 
         $("#container").append(row);
-}
-}
- createRows();
+    };
+};
 
- var makeDots = function() {
-     for(var i = 0; i < 19; i ++) {
-        var ul = $("<p>").addClass("col-12").text("I ate so many fucking dicks today")
-        timeBlock.append(ul)
-     }
- }
+$("#container").on("click", "button", function() {
+    var textContent = [
+        $("#textarea0").val(),
+        $("#textarea1").val(),
+        $("#textarea2").val(),
+        $("#textarea3").val(),
+        $("#textarea4").val(),
+        $("#textarea5").val(),
+        $("#textarea6").val(),
+        $("#textarea7").val(),
+        $("#textarea8").val(),
+        $("#textarea9").val(),
+        $("#textarea10").val(),
+        $("#textarea11").val()
+    ]
+    localStorage.setItem("tasks", JSON.stringify(textContent))
+});
+
+var loadText = function() {
+    var text = JSON.parse(localStorage.getItem("tasks"));
+    if(text) {
+        textArray = text
+    }
+};
+
+var startScheduler = function () {
+    loadText();
+    setDate();
+     createRows();
+     pasCurFut();
+};
+
+startScheduler();
